61d4e5eb886ddace58fb5b8a95226e1b10d7f27a
Anton Chekulaev <metaphore@bk.ru>
2019-07-11 14:48:09 +1000

    Web Demo canvas in now properly resizeble.

--- demo/html/src/com/crashinvaders/vfx/demo/client/HtmlLauncher.java
+++ demo/html/src/com/crashinvaders/vfx/demo/client/HtmlLauncher.java
@@ -1,49 +1,42 @@
 package com.crashinvaders.vfx.demo.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.crashinvaders.vfx.demo.App;
 import com.crashinvaders.vfx.gwt.GwtGLExtCalls;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
+import com.google.gwt.user.client.Window;
 
 public class HtmlLauncher extends GwtApplication {
 
-        // USE THIS CODE FOR A FIXED SIZE APPLICATION
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
-        // END CODE FOR FIXED SIZE APPLICATION
+         private static final int PADDING = 0;
+         private GwtApplicationConfiguration cfg;
 
-        // UNCOMMENT THIS CODE FOR A RESIZABLE APPLICATION
-        // PADDING is to avoid scrolling in iframes, set to 20 if you have problems
-        // private static final int PADDING = 0;
-        // private GwtApplicationConfiguration cfg;
-        //
-        // @Override
-        // public GwtApplicationConfiguration getConfig() {
-        //     int w = Window.getClientWidth() - PADDING;
-        //     int h = Window.getClientHeight() - PADDING;
-        //     cfg = new GwtApplicationConfiguration(w, h);
-        //     Window.enableScrolling(false);
-        //     Window.setMargin("0");
-        //     Window.addResizeHandler(new ResizeListener());
-        //     cfg.preferFlash = false;
-        //     return cfg;
-        // }
-        //
-        // class ResizeListener implements ResizeHandler {
-        //     @Override
-        //     public void onResize(ResizeEvent event) {
-        //         int width = event.getWidth() - PADDING;
-        //         int height = event.getHeight() - PADDING;
-        //         getRootPanel().setWidth("" + width + "px");
-        //         getRootPanel().setHeight("" + height + "px");
-        //         getApplicationListener().resize(width, height);
-        //         Gdx.graphics.setWindowedMode(width, height);
-        //     }
-        // }
-        // END OF CODE FOR RESIZABLE APPLICATION
+         @Override
+         public GwtApplicationConfiguration getConfig() {
+             int w = Window.getClientWidth() - PADDING;
+             int h = Window.getClientHeight() - PADDING;
+             cfg = new GwtApplicationConfiguration(w, h);
+             Window.enableScrolling(false);
+             Window.setMargin("0");
+             Window.addResizeHandler(new ResizeListener());
+             cfg.preferFlash = false;
+             return cfg;
+         }
+
+         class ResizeListener implements ResizeHandler {
+             @Override
+             public void onResize(ResizeEvent event) {
+                 int width = event.getWidth() - PADDING;
+                 int height = event.getHeight() - PADDING;
+                 getRootPanel().setSize(width + "px", height + "px");
+                 getApplicationListener().resize(width, height);
+                 Gdx.graphics.setWindowedMode(width, height);
+             }
+         }
 
         @Override
         public ApplicationListener createApplicationListener () {
